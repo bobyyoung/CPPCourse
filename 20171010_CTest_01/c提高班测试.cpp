ab14d4c259f91ade27e06f1498f576f5f8af6a5d
yangjiabo <yangjiabo@ZB-R304B3BY>
2017-12-18 10:51:06 +0800

    添加项目文件。

---
+++ 20171010_CTest_01/c提高班测试.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int main(int argc, char * argv[])
+{
+	int a;
+	int *p;
+
+	a = 100;
+	p = &a;
+
+	cout << "*P:" << *p << endl;
+	cout << "&P:" << &p << endl;
+	cout << "P:" << p << endl;
+	cout << "a:" << a << endl;
+	cout << "&a:" << &a << endl;
+
+	getchar();
+
+
+} 
